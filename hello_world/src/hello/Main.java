e00807375bf270fe2629db6cfa4723b73b13e747
Sunghwan1234 <sunghwan1002222@gmail.com>
2024-01-17 20:41:30 -0600

    fart¹⁵ update jump a little more I guess

--- hello_world/src/hello/Main.java
+++ hello_world/src/hello/Main.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main() {
         System.out.println("Hello, world!");
     }
 }
--- jump/src/jump/Blocks.java
+++ jump/src/jump/Blocks.java
@@ -11,8 +11,10 @@ public class Blocks {
 
     // Blocks 2D Array
     // the internal array settings: posX, posY, type
-    // type: spike or block, 0 or 1
+    // SPIKE: 1
+    // BLOCK: 2
     public double block[][] = new double[120][3];
+    public double level[][] = new double[100][100];
     public double defY;
 
     public Blocks(int defY) { // setup and nothing
@@ -24,7 +26,7 @@ public class Blocks {
             double pby = p.posY+Player.HEIGHT; // player bottom y
             double prx = p.posX+Player.WIDTH; // player right x
 
-        if (t==1) { // BLOCK COLLISION
+        if (t==2) { // BLOCK COLLISION
             // block X and Y is bottom left
 
             // quick block variables
@@ -43,7 +45,7 @@ public class Blocks {
                 p.onground = true;
                 System.out.println("block.collision:inblock");
             }
-        } else if (t==0) { // SPIKE COLLISION
+        } else if (t==1) { // SPIKE COLLISION
             // checking for player collision by a smaller hitbox instead of scanning
             // hitbox should be (x+6,y),(x+w-6,y),(x+w-6,y+h-8),(x+6,y+h-8)
             // player hitbox is 20x20 left top is px
@@ -70,23 +72,23 @@ public class Blocks {
         }}
 
     public void generate() {
-        block[0][0] = 750; block[0][1] = defY; block[0][2] = 1;
+        block[0][0] = 750; block[0][1] = defY; block[0][2] = 0;
 
         for(int i=1; i<100; i++) {
             block[i][1] = defY;// block[i][0] = block[i-1][0]+20; block[i][2]=0;
 
             
             // testing stuff
-            block[i][0] = block[i-1][0]+20; block[i][2] = 1;
+            block[i][0] = block[i-1][0]+20; block[i][2] = 2;
             i+=1;
 
-            block[i][0] = block[i-1][0]+20; block[i][1] = defY; block[i][2] = 0;
+            block[i][0] = block[i-1][0]+20; block[i][1] = defY; block[i][2] = 1;
             i+=1;
-            block[i][0] = block[i-1][0]+20; block[i][1] = defY; block[i][2] = 0;
+            block[i][0] = block[i-1][0]+20; block[i][1] = defY; block[i][2] = 1;
             i+=1;
-            block[i][0] = block[i-1][0]+20;block[i][1] = defY; block[i][2] = 0;
+            block[i][0] = block[i-1][0]+20;block[i][1] = defY; block[i][2] = 1;
             i+=1;
-            block[i][0] = block[i-1][0]+20; block[i][1] = defY; block[i][2] = 0;
+            block[i][0] = block[i-1][0]+20; block[i][1] = defY; block[i][2] = 1;
               
         }
         System.out.println("spikes.generation:complete");
@@ -101,20 +103,20 @@ public class Blocks {
 
 
             // spike drawing
-            if (block[i][2] == 0) {
+            if (block[i][2] == 1) {
                 g.setColor(Color.RED);
                 g.fillPolygon(
                     new int[] {(int) block[i][0], (int) block[i][0]+BaseWidth, (int) (block[i][0]+BaseWidth/2)}
                     ,new int[] {(int) block[i][1], (int) block[i][1], (int) block[i][1]-Height}
                     ,3
                 );
-            } else if (block[i][2] == 1) {
+            } else if (block[i][2] == 2) {
                 // block drawing
                 g.setColor(Color.YELLOW);
                 g.fillRect((int) block[i][0], (int) block[i][1]-Height, (int) BaseWidth, (int) Height);
             }
 
-            if (block[99][0] < 0) { // WINNING
+            if (block[100][0] < 0) { // WINNING
                 System.out.println("You made it to the end. Congrats.");
                 
                 System.exit(1);
